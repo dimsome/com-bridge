ec57bdf6c717c75ed6b117e5bb9973b822e5a51b
kodam <kolozsvariadam@gmail.com>
2023-11-18 00:38:51 +0300

    feat: add Modal

---
+++ packages/app/app/components/dialog/Modal.tsx
@@ -0,0 +1,56 @@
+import {Dialog, Transition} from '@headlessui/react'
+import {Fragment, ReactNode} from 'react'
+
+type ModalProps = { render: (closeModal: () => void) => ReactNode, closeOnBarrierClick?: boolean } & InternalModalProps
+export default function Modal({render, isOpen, setIsOpen, closeOnBarrierClick = false}: ModalProps) {
+    function closeModal() {
+        setIsOpen(false)
+    }
+
+    return (
+
+        <Transition appear show={isOpen} as={Fragment}>
+            <Dialog as="div" className="relative z-40" onClose={() => {
+                closeOnBarrierClick && closeModal()
+            }}>
+                <div className="fixed inset-0 bg-black/30" aria-hidden="true"/>
+
+                <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                >
+                    <div className="fixed inset-0 bg-black bg-opacity-25"/>
+                </Transition.Child>
+
+                <div className="fixed inset-0 overflow-y-auto">
+                    <div className="flex min-h-full items-center justify-center p-4">
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0 scale-95"
+                            enterTo="opacity-100 scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 scale-100"
+                            leaveTo="opacity-0 scale-95"
+                        >
+                            <Dialog.Panel
+                                className="w-full max-w-md transform overflow-hidden p-0 bg-transparent align-middle transition-all">
+                                {render(closeModal)}
+                            </Dialog.Panel>
+                        </Transition.Child>
+                    </div>
+                </div>
+            </Dialog>
+        </Transition>
+    )
+}
+
+export type InternalModalProps = {
+    isOpen: boolean;
+    setIsOpen: (isOpen: boolean) => void;
+}
