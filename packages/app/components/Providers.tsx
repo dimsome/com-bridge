7a4fea73e95a32099d0f046b11caaad9da80bd78
kodam <kolozsvariadam@gmail.com>
2023-11-18 17:36:38 +0300

    feat: add more testnetes

--- packages/app/components/Providers.tsx
+++ packages/app/components/Providers.tsx
@@ -4,14 +4,14 @@ import { ReactNode } from "react";
 import { WagmiConfig, configureChains, createConfig } from "wagmi";
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 // tesnets
-import { hardhat, scrollSepolia, polygonZkEvmTestnet, mantleTestnet } from "wagmi/chains";
+import { hardhat, scrollSepolia, polygonZkEvmTestnet, mantleTestnet, sepolia, avalancheFuji } from "wagmi/chains";
 // mainnets
 import { polygonZkEvm, mantle } from "wagmi/chains";
 
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 
-const testnets = [scrollSepolia, polygonZkEvmTestnet, mantleTestnet]
+const testnets = [sepolia, avalancheFuji, scrollSepolia, polygonZkEvmTestnet, mantleTestnet]
 const mainnets = [polygonZkEvm, mantle] //scroll not yet live
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
