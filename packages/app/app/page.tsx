3c81d476494e120eaad06487a2c0d814d29d49f1
kodam <kolozsvariadam@gmail.com>
2023-11-18 16:02:31 +0300

    feat: use homeform insted of landing page if a wallet is connected

--- packages/app/app/page.tsx
+++ packages/app/app/page.tsx
@@ -1,16 +1,22 @@
 "use client";
-import { Wrapper } from "@/components/Wrapper";
-import React from "react";
+import {Wrapper} from "@/components/Wrapper";
+import React, {useMemo} from "react";
 import Landing from "@/components/landing/Landing";
+import {useAccount} from "wagmi";
+import {HomeForm} from "@/components/home/HomeForm";
+import {useHydrated} from "@/src/hooks/useHydrated";
 
 const Home = () => {
-  return (
-    <main>
-      <Wrapper>
-        <Landing />
-      </Wrapper>
-    </main>
-  );
+    const {isConnecting, address} = useAccount();
+    const isHydrated = useHydrated();
+    const showLanding = useMemo(() => !address || isConnecting || !isHydrated, [address, isConnecting, isHydrated]);
+    return (
+        <main>
+            <Wrapper>
+                {showLanding ? <Landing/> : <HomeForm/>}
+            </Wrapper>
+        </main>
+    );
 };
 
 export default Home;
